49c6ab741b00e77970bc5533048154119aa5ae14
Igor Sokolov <igorgrru@gmail.com>
2017-09-29 04:40:42 +0700

    Supported totalCollected public variable in PapyrusPrototypeToken smart contract.

--- README.md
+++ README.md
@@ -8,18 +8,29 @@ Also we use [MultiSigWalletWithDailyLimit](https://github.com/gnosis/MultiSigWal
 
 ## PapyrusPrototypeToken.sol
 
-Implements ERC20 token. It is inherited from [OpenZeppelin](https://github.com/OpenZeppelin/zeppelin-solidity) smart contract `MintableToken`.
+Implements ERC20 token. It is inherited from [OpenZeppelin](https://github.com/OpenZeppelin/zeppelin-solidity) `StandardToken` and `Ownable` smart contracts.
 
 It has hardcoded name **Papyrus Prototype Token** and symbol **PRP** with **18 decimals** (just like ETH and many other tokens).
 
 PRP token is not transferable from the creation. This can be changed later using function `setTransferable()` by *owner* of smart contract.
 
+Smart contract has public variable `totalCollected`. It accumulates amount of received USD during Token Generation Event. To calculate this value in USD when calling `mint` function we specify `_priceUsd` value used at moment of participation. Value **0** can be used for tokens that are minted not as part of Token Generation Event (tokens minted for bounty, etc.).
+
 #### Events
 
+- `Mint` - arises when tokens are minted. Has following arguments:
+  - `to` (`address`) - address which gets minted tokens.
+  - `amount` (`uint256`) - amount of minted tokens.
+- `MintFinished` - arises when minting tokens is over. It is not possible to mint tokens after event is happened.
 - `TransferableChanged` - arises when ability to transfer tokens by holders is changed. Has following arguments:
   - `transferable` (`bool`) - new transferable value.
 
 #### Functions
 
+- `mint` - mints (creates) tokens and store them at specified address. Can be used only by *owner* until minting is finished. Has following arguments:
+  - `_to` (`address`) - address which gets minted tokens.
+  - `_amount` (`uint256`) - amount of minted tokens.
+  - `_priceUsd` (`uint256`) - price for 1 token in USD. Please note that 18 decimals value is expected. For example, if price for 1 token is $0.8 then value as 0.8 ETH in weis should be used.
+- `finishMinting` - finishes minting and denying further minting tokens. Can be used only by *owner* until minting is finished.
 - `setTransferable` - changes ability to transfer tokens by holders. Can be used only by *owner*. Has following arguments:
   - `_transferable` (`bool`) - token becomes transferable when `true` and not transferable when `false`.
--- contracts/PapyrusPrototypeToken.sol
+++ contracts/PapyrusPrototypeToken.sol
@@ -1,12 +1,15 @@
 pragma solidity ^0.4.15;
 
-import "./zeppelin/token/MintableToken.sol";
+import "./zeppelin/ownership/Ownable.sol";
+import "./zeppelin/token/StandardToken.sol";
 
 /// @title Papyrus Prototype Token (PRP) smart contract.
-contract PapyrusPrototypeToken is MintableToken {
+contract PapyrusPrototypeToken is StandardToken, Ownable {
 
     // EVENTS
 
+    event Mint(address indexed to, uint256 amount);
+    event MintFinished();
     event TransferableChanged(bool transferable);
 
     // PUBLIC FUNCTIONS
@@ -15,23 +18,55 @@ contract PapyrusPrototypeToken is MintableToken {
     function() { revert(); }
 
     // Check transfer ability and sender address before transfer
-    function transfer(address _to, uint _value) public canTransfer returns (bool) {
+    function transfer(address _to, uint _value) canTransfer public returns (bool) {
         return super.transfer(_to, _value);
     }
 
     // Check transfer ability and sender address before transfer
-    function transferFrom(address _from, address _to, uint _value) public canTransfer returns (bool) {
+    function transferFrom(address _from, address _to, uint _value) canTransfer public returns (bool) {
         return super.transferFrom(_from, _to, _value);
     }
 
+    /// @dev Function to mint tokens.
+    /// @param _to The address that will receive the minted tokens.
+    /// @param _amount The amount of tokens to mint.
+    /// @param _priceUsd The price of minted token at moment of purchase in USD with 18 decimals.
+    /// @return A boolean that indicates if the operation was successful.
+    function mint(address _to, uint256 _amount, uint256 _priceUsd) onlyOwner canMint public returns (bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        if (_priceUsd != 0) {
+            uint256 amountUsd = _amount.mul(_priceUsd).div(10**18);
+            totalCollected = totalCollected.add(amountUsd);
+        }
+        Mint(_to, _amount);
+        Transfer(0x0, _to, _amount);
+        return true;
+    }
+
+    /// @dev Function to stop minting new tokens.
+    /// @return A boolean that indicates if the operation was successful.
+    function finishMinting() onlyOwner canMint public returns (bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
+
     /// @dev Change ability to transfer tokens by users.
-    function setTransferable(bool _transferable) public onlyOwner {
+    /// @return A boolean that indicates if the operation was successful.
+    function setTransferable(bool _transferable) onlyOwner public returns (bool) {
         require(transferable != _transferable);
         transferable = _transferable;
         TransferableChanged(transferable);
+        return true;
     }
 
     // MODIFIERS
+
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
 
     modifier canTransfer() {
         require(transferable || msg.sender == owner);
@@ -48,4 +83,10 @@ contract PapyrusPrototypeToken is MintableToken {
 
     // At the start of the token existence token is not transferable
     bool public transferable = false;
+
+    // Will be set to true when minting tokens will be finished
+    bool public mintingFinished = false;
+
+    // Amount of USD (with 18 decimals) collected during sale phase
+    uint public totalCollected;
 }
